bab317da44d851089a3a89036fa4370cccd33ff7
Mathis <maserrie@student.42perpigan.fr>
2023-04-10 22:14:53 +0200

    push

--- srcs/ft_signal.c
+++ srcs/ft_signal.c
@@ -6,7 +6,7 @@
 /*   By: maserrie <maserrie@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 12:23:15 by maserrie          #+#    #+#             */
-/*   Updated: 2023/04/10 22:10:42 by maserrie         ###   ########.fr       */
+/*   Updated: 2023/04/10 22:14:27 by maserrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	ft_signal(int sig)
 	if (sig == SIGQUIT)
 	{
 		rl_replace_line("\n", 0);
-		ft_putstr_fd("exit\n", 0);
+		ft_putstr_fd("exit\n", 1);
 	}
 }
 
