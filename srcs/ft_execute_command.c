91f6cf71d03c937607edc07c20adf3b58b9b359c
Adrien Mori <adrienmori@MacBook-Adrien-Mori.local>
2023-04-13 21:16:16 +0200

    d

--- srcs/ft_execute_command.c
+++ srcs/ft_execute_command.c
@@ -6,55 +6,12 @@
 /*   By: adrienmori <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 19:59:43 by adrienmori        #+#    #+#             */
-/*   Updated: 2023/04/13 21:14:01 by adrienmori       ###   ########.fr       */
+/*   Updated: 2023/04/13 21:15:29 by adrienmori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 #define READ_OUTPUT_BUFFER_SIZE 128
-
-static void	read_output(char **out, int pipes[2][2])
-{
-	char	*buffer;
-	int		size;
-
-	*out = NULL;
-	buffer = (char *)malloc(sizeof(char) * (READ_OUTPUT_BUFFER_SIZE + 1));
-	if (!buffer)
-		return ;
-	size = read(pipes[1][0], buffer, READ_OUTPUT_BUFFER_SIZE);
-	while (size > 0)
-	{
-		buffer[size] = 0;
-		if (!(*out) && buffer[0] == ' ')
-			ft_str_realloc(out, buffer + 1);
-		else
-			ft_str_realloc(out, buffer);
-		size = read(pipes[1][0], buffer, READ_OUTPUT_BUFFER_SIZE);
-	}
-	free(buffer);
-}
-
-static void	start_execve(t_env *env, char **cmd, int pipes[2][2], char *input)
-{
-	close(pipes[0][1]);
-	close(pipes[1][0]);
-	if (input)
-		dup2(pipes[0][0], STDIN_FILENO);
-	dup2(pipes[1][1], STDOUT_FILENO);
-	close(pipes[0][0]);
-	close(pipes[1][1]);
-	write(STDOUT_FILENO, " ", 1);
-}
-
-void	execute_builtin_function(t_env *env, char **cmd_with_args)
-{
-	return ;
-}
-int	is_builtin(char *cmd)
-{
-	return (1);
-}
 
 void	ft_execute(t_env *env, char *cmd, char *input)
 {
@@ -70,8 +27,6 @@ void	ft_execute(t_env *env, char *cmd, char *input)
 	cmd_with_args = ft_split(cmd, ' ');
 	if (!cmd_with_args)
 		return ;
-	if (is_builtin(cmd_with_args[0]))
-		execute_builtin_function(env, cmd_with_args);
 	ft_printf("ok");
 	return ;
 }
